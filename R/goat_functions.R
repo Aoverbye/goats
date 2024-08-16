02e243c11a6d7bc4463fd946b36fc2f4d6922c7a
Aoverbye <amandaoverbye@gmail.com>
2024-08-16 15:27:57 -0500

    Initial commit

---
+++ R/goat_functions.R
@@ -0,0 +1,17 @@
+
+#' A tragic function that I do not know how to get to work and doesnt
+#' follow the assignment, but I am trying my best :)
+#'
+#' @param K number of goat score 1
+#' @param r number of goat score 2
+#'
+#' @return the total score for got in this imaginary competition
+#' @export
+#'
+#' @examples
+prized_goat <- function(K, r) {
+  prized_goat() <- (K * r) / 3
+  return(prized_goat())
+}
+
+
